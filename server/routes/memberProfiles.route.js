86af4400790f47d787942796051c8267af7c6360
Azrael Morherudaen <72979466+draconiis72@users.noreply.github.com>
2021-02-16 22:38:19 -0600

    begining changes

---
+++ server/routes/memberProfiles.route.js
@@ -0,0 +1,11 @@
+const AnimeController = require('../controllers/animeMovies.controller');
+
+
+module.exports = (app) =>{
+    app.get('/api/animeMovies', AnimeController.FindallAnimeMovies);
+    app.get('/api/animeMovies/:id', AnimeController.FindOneAnimeMovie);
+    app.post('/api/animeMovies', AnimeController.CreateOneAnimeMovie);
+    app.put('/api/animeMovies/:id', AnimeController.UpdateExistingAnimeMovie);
+    app.delete('/api/animeMovies/:id', AnimeController.DeleteExistingAnimeMovie);
+    
+}
